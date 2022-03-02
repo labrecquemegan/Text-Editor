ede9813e4a4312c50ddd4b240e8949d1f2c28e79
ML <labrecquemegan@hotmail.com>
2022-03-01 19:10:48 -0500

    changed a bit of webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = () => {
       // Injects our custom service worker
       new InjectManifest({
         swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
+        swDest: "service-worker.js",
       }),
 
       // Creates a manifest.json file.
